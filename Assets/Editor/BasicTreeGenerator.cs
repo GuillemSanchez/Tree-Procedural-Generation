a0d18ee91953c48ed4e797fe134c6302350db90a
GuillemSanchez <36293100+GuillemSanchez@users.noreply.github.com>
2022-05-03 13:32:42 +0200

    New Structure for the base and new methods for creation

--- Assets/Editor/BasicTreeGenerator.cs
+++ Assets/Editor/BasicTreeGenerator.cs
@@ -6,6 +6,12 @@ using TreeEditor;
 using UnityEngine.UIElements;
 public class BasicTreeGenerator : EditorWindow {
 
+    // window vars
+    bool treeWindow = true;
+    bool rootWindow = true;
+    bool trunkWindow = false;
+
+    bool rootSelected = false;
 
     // Core Variables
     Tree tree;
@@ -24,23 +30,36 @@ public class BasicTreeGenerator : EditorWindow {
     [MenuItem("Tree Procedural Generation/BasicTreeGenerator")]
     private static void ShowWindow() {
         var window = GetWindow<BasicTreeGenerator>();
-        window.titleContent = new GUIContent("BasicTreeGenerator");
+        window.titleContent = new GUIContent("Tree Base");
         window.Show();
-        // Primero que pongan el arbol despues que se cargan los valors del arbol y salen las opciones
+
     }
 
     private void OnGUI() {
-        GUILayout.Label("test de arbol", EditorStyles.boldLabel);
-        tree = EditorGUILayout.ObjectField("tree object", tree, typeof(Tree), true) as Tree;
-        seed = EditorGUILayout.IntSlider("Seed",seed,1,999999);
-        areaSpreed = EditorGUILayout.Slider("Area Spread",areaSpreed,0,10);
-        groundOffset = EditorGUILayout.Slider("Ground OffSet",groundOffset,0,10);
-        LODoverall = EditorGUILayout.Slider("LOD quality Overall", LODoverall, 0, 1);
+        treeWindow = EditorGUILayout.Foldout(treeWindow, "Select a Base");
+        if (treeWindow)
+        {
+            ShowTreeBasic();
+        }
+        if (rootSelected){
+            rootWindow = EditorGUILayout.Foldout(rootWindow, "Root Basics");
+
+            if (rootWindow)
+            {
+                ShowRootBasics();
+            }
+
+            // Button to adding the trunk?
+            if (GUILayout.Button("AddTrunk")){
+                AddMainTrunk();
+            }
+        }
     }
 
     void OnInspectorUpdate()
-    {
-        ChangeRoot();
+    {   
+        if (rootSelected && rootWindow)
+            ChangeRoot();
     }
 
     private void ChangeRoot() {
@@ -58,10 +77,46 @@ public class BasicTreeGenerator : EditorWindow {
         advancedData.UpdateSeed(seed);
         Material[] materials;
         advancedData.UpdateMesh(tree.transform.worldToLocalMatrix, out materials);
+    } 
+
+    private void ShowTreeBasic() {
+
+        // In this function we want the developer to put first the tree base
+        tree = EditorGUILayout.ObjectField("tree object", tree, typeof(Tree), true) as Tree;
+        if (tree != null) {
+            rootSelected = true;
+        }
+        else {
+            rootSelected = false;
+        }
+    }
+
+    private void ShowRootBasics() {
+        // Basics of the Root of a Tree
+        seed = EditorGUILayout.IntSlider("Seed",seed,1,999999);
+        areaSpreed = EditorGUILayout.Slider("Area Spread",areaSpreed,0,10);
+        groundOffset = EditorGUILayout.Slider("Ground OffSet",groundOffset,0,10);
+        LODoverall = EditorGUILayout.Slider("LOD quality Overall", LODoverall, 0, 1);
+    }
+
+    private void AddMainTrunk() {
+        advancedData = tree.data as TreeData;
+
+        advancedData.AddGroup(advancedData.root, typeof(TreeGroupBranch));
+        Material[] materials;
+        advancedData.UpdateMesh(tree.transform.worldToLocalMatrix, out materials);
+        AssignMaterials(tree.GetComponent<Renderer>(), materials);
+    }
 
 
+    private void AssignMaterials(Renderer renderer, Material[] materials){
+        if (renderer != null)
+        {
+            if (materials == null)
+                materials = new Material[0];
 
-
+            renderer.sharedMaterials = materials;
+        }
     }
 }
 
